4bb75fff550974583b29bf0e85c28330aed8aa20
Edward Zhang <edward@zhang.nz>
2020-08-28 18:54:58 +1200

    Initial Commit

---
+++ gpt2bot.py
@@ -0,0 +1,79 @@
+from telegram.ext import Updater, CommandHandler
+import subprocess
+
+gpt2tcThreads = 1
+allowedChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!+-?,.' "
+
+def filtertext(text):
+    return ''.join(filter(lambda x: x in allowedChars, text.replace('\n', ' ').strip()))
+
+def gpt2tc(text, modelSize, modelName):
+    cleanInput = filtertext(text)
+    output = subprocess.run(["gpt2tc", "-m", modelSize, "-f", modelName, "-T", str(gpt2tcThreads), "g", cleanInput], text=True, capture_output=True)
+    if output.returncode == 0:
+        gptoutput = output.stdout
+        cutoutput = gptoutput[:gptoutput.find("<|endoftext|>")]
+        return cutoutput
+    else:
+        return "Error"
+
+def hello(update, context):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+def parse_to_query(update):
+    split = update.message.text.split(' ', 1)
+    return split[1] if len(split) > 1 else ''
+
+def austin(update, context):
+    update.message.reply_text(
+        gpt2tc(parse_to_query(update), "345M", "gpt2_austinmedium.bin"),
+        quote = True)
+
+def terran(update, context):
+    update.message.reply_text(
+        gpt2tc(parse_to_query(update), "345M", "gpt2_terranmedium.bin"),
+        quote = True)
+
+def sid(update, context):
+    update.message.reply_text(
+        gpt2tc(parse_to_query(update), "345M", "gpt2_sidmedium.bin"),
+        quote = True)
+
+def mohan(update, context):
+    update.message.reply_text(
+        gpt2tc(parse_to_query(update), "345M", "gpt2_mohanmedium.bin"),
+        quote = True)
+
+def edward(update, context):
+    update.message.reply_text(
+        gpt2tc(parse_to_query(update), "345M", "gpt2_edwardmedium.bin"),
+        quote = True)
+
+def generic(update, context):
+    update.message.reply_text(
+        gpt2tc(parse_to_query(update), "345M", "gpt2_genericmedium.bin"),
+        quote = True)
+
+def henry(update, context):
+    update.message.reply_text(
+        gpt2tc(parse_to_query(update), "117M", "gpt2_henrysmall.bin"),
+        quote = True)
+
+key = ''
+with open ("telegramkey.txt", "r") as config:
+    key = config.readlines()[0]
+
+updater = Updater(key, use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('mohan', mohan))
+updater.dispatcher.add_handler(CommandHandler('edward', edward))
+updater.dispatcher.add_handler(CommandHandler('terran', terran))
+updater.dispatcher.add_handler(CommandHandler('sid', sid))
+updater.dispatcher.add_handler(CommandHandler('austin', austin))
+updater.dispatcher.add_handler(CommandHandler('generic', generic))
+updater.dispatcher.add_handler(CommandHandler('henry', henry))
+
+updater.start_polling()
+updater.idle()
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+pip install -r requirements.txt
+python gpt2bot.py
